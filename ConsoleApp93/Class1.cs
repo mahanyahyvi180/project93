ea54ace5662148a5a5c1a3beeab3a7a217b87fe7
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-13 19:51:43 +0330

    Add files via upload

---
+++ ConsoleApp93/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp93
+{
+    public class BasePlusCommissionEmployee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+        private decimal grossSales;
+        private decimal commissionRate;
+        private decimal baseSalary;
+
+        public BasePlusCommissionEmployee(string firstName, string lastName, string socialSecurityNumber, decimal grossSales, decimal commissionRate, decimal baseSalary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+            GrossSales = grossSales;
+            CommissionRate = commissionRate;
+            Basesalary = baseSalary;
+        }
+
+        public decimal GrossSales
+        {
+            get {
+                return grossSales;
+            }
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(GrossSales)} must be >=0");
+                }
+
+                grossSales = value;
+            }
+        }
+
+        public decimal CommissionRate
+        {
+            get
+            {
+                return commissionRate;
+            }
+            set
+            {
+
+                if (value <= 0 || value >= 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(CommissionRate)}must be <=0 and >=1");
+                }
+                commissionRate = value;
+            }
+        }
+
+        public decimal Basesalary
+        {
+            get
+            {
+                return baseSalary;
+            }
+            set
+            {
+
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(Basesalary)}must be >=0");
+                }
+                baseSalary = value;
+            }
+        }
+
+        public decimal Earnings() => baseSalary + (commissionRate * grossSales);
+
+        public override string ToString() => $"base-salaried commission employee:{FirstName}{LastName}\n" + $"social security number:{SocialSecurityNumber}\n" + $"gross sales :{grossSales:C}\n" + $"commission rate:{commissionRate:F2}\n" + $"base salary:{baseSalary:C}";
+    
+            
+        
+
+    }
+}
---
+++ ConsoleApp93/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp93
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new BasePlusCommissionEmployee("Bob", "Lewis", "333-33-3333", 5000.00M, .04M, 300.00M);
+
+            Console.WriteLine("Employee information obtained by properties and methods:\n");
+            Console.WriteLine($"First name is{employee.FirstName}");
+            Console.WriteLine($"Last name is{employee.LastName}");
+            Console.WriteLine($"Social security number is{employee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross sales are{employee.GrossSales:C}");
+            Console.WriteLine($"Commission rate is{employee.CommissionRate:F2}");
+            Console.WriteLine($"Earnings are{employee.Earnings():C}");
+            Console.WriteLine($"Base salary is {employee.Basesalary:C}");
+            employee.Basesalary = 1000.00M;
+
+            Console.WriteLine("\nUpdated employee information obtained by ToString:\n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"earnings:{employee.Earnings():C}");
+            Console.ReadLine();
+        }
+    }
+}
